7e89e70bbaf19733b7304cad105bfb461da16e6d
AdarshGrewal <adarshgrewal@gmail.com>
2026-06-02 18:05:29 +0200

    NX1: Provide liboemcrypto linker config
    Change-Id: Ib7c242be5db626ba0151bbf60dd6d60d7590bc46
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -45,6 +45,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.mediatek \
     android.hardware.boot-service.mediatek_recovery
 
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    $(LOCAL_PATH)/configs/linker/linker.config.json
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
