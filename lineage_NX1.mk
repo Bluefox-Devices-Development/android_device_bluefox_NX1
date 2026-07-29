2d17150a0de326a13632fff645ab21a12711faad
EdwardWu <bluehome.wu@gmail.com>
2026-07-30 01:05:46 +0800

    NX1: Enable insecure ADB for bring-up
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- lineage_NX1.mk
+++ lineage_NX1.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/bluefox/NX1/device.mk)
 
+# Keep ADB available while the bootloader reports a spoofed locked state.
+WITH_ADB_INSECURE := true
+
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
